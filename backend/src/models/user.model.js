5360ec9bc2ae7aec44b2b451fc9e3efb7ae5f6cc
Khemraj <khemrajgupta10@gmail.com>
2026-05-08 11:24:04 +0530

    added database uri and currently there's no data

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -2,12 +2,10 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
   {
-    username: { type: String, required: true, unique: true },
-    email: { type: String, required: true, unique: true },
+    username: { type: String, required: true, unique: true, trim: true },
+    email: { type: String, required: true, unique: true, lowercase: true },
     password: { type: String, required: true },
     role: { type: String, enum: ["user", "admin"], default: "user" },
-    createdAt: { type: Date, default: Date.now },
-    updatedAt: { type: Date, default: Date.now },
   },
   {
     timestamps: true,
